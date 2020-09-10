adbbb4bc4e311967b5636ea8a8cd92fd4c7dabb1
Mitya Urchenko <mitya.urchenko@gmail.com>
2020-09-10 23:39:10 +0300

    commit

--- bitrix-d7/methods.php
+++ bitrix-d7/methods.php
@@ -20,35 +20,39 @@ $dbItems = Iblock\ElementTable::getList(array(
 'select' => array('ID', 'NAME', 'IBLOCK_ID'),
 'filter' => array('IBLOCK_ID' => $arIblock['ID'])
 ));
+
 while ($arItem = $dbItems->fetch()){
-// свойства элементов
-$dbProperty = \CIBlockElement::getProperty(
-$arItem['IBLOCK_ID'],
-$arItem['ID']
-);
-while($arProperty = $dbProperty->Fetch()){
-$arItem['PROPERTIES'][] = $arProperty;
+
+    // собирает массив свойств
+    $dbProperty = \CIBlockElement::getProperty(
+        $arItem['IBLOCK_ID'],
+        $arItem['ID']
+    );
+
+    // записывает в массив элемента массив свойств
+    while($arProperty = $dbProperty->Fetch()){
+        $arItem['PROPERTIES'][] = $arProperty;
+    }
 }
-}
 
 
 
-$dbItems = Iblock\ElementTable::add($data); // добавление элемента в инфоблок
-$dbItems = Iblock\ElementTable::addMulti($rows, $ignoreEvents = false); // для множественного добавления записей
-$dbItems = Iblock\ElementTable::checkFields($result, $primary, $data); //проверяет поля данных перед записью в БД.
-$dbItems = Iblock\ElementTable::delete($id); // удаление элемента по ID.ЗАБЛОКИРОВАН!
-$dbItems = Iblock\ElementTable::getById($id); // получение элемента по ID
-$dbItems = Iblock\ElementTable::getByPrimary($primary, $parameters = array()); //возвращает выборку по первичному ключу сущности и по опциональным параметрам
-$dbItems = Iblock\ElementTable::getConnectionName(); //возвращает имя соединения для сущности
-$dbItems = Iblock\ElementTable::getCount($filter = array(), $cache = array()); //выполняет COUNT запрос к сущности и возвращает результат
-$dbItems = Iblock\ElementTable::getEntity(); //возвращает объект сущности.
-$dbItems = Iblock\ElementTable::getList($parameters = array()); // получение элементов
-$dbItems = Iblock\ElementTable::getMap(); //возвращает описание карты сущностей
-$dbItems = Iblock\ElementTable::getRow($parameters); //возвращает один столбец (или null) по параметрам
-$dbItems = Iblock\ElementTable::getRowById($id); //возвращает один столбец (или null) по первичному ключу сущности
-$dbItems = Iblock\ElementTable::getTableName(); //возвращает имя таблицы БД для сущности
-$dbItems = Iblock\ElementTable::query(); //создаёт и возвращает объект запроса для сущности
-$dbItems = Iblock\ElementTable::update($primary, $data); // обновление элемента по ID
-$dbItems = Iblock\ElementTable::updateMulti($ids, $data, $ignoreEvents = false);
-$dbItems = Iblock\ElementTable::enableCrypto($field, $table = null, $mode = true); //устанавливает флаг поддержки шифрования для поля
-$dbItems = Iblock\ElementTable::cryptoEnabled($field, $table = null); //возвращает true если шифрование разрешено для поля
+Iblock\ElementTable::add($data); // добавление элемента в инфоблок
+Iblock\ElementTable::addMulti($rows, $ignoreEvents = false); // для множественного добавления записей
+Iblock\ElementTable::checkFields($result, $primary, $data); //проверяет поля данных перед записью в БД.
+Iblock\ElementTable::delete($id); // удаление элемента по ID.ЗАБЛОКИРОВАН!
+Iblock\ElementTable::getById($id); // получение элемента по ID
+Iblock\ElementTable::getByPrimary($primary, $parameters = array()); //возвращает выборку по первичному ключу сущности и по опциональным параметрам
+Iblock\ElementTable::getConnectionName(); //возвращает имя соединения для сущности
+Iblock\ElementTable::getCount($filter = array(), $cache = array()); //выполняет COUNT запрос к сущности и возвращает результат
+Iblock\ElementTable::getEntity(); //возвращает объект сущности.
+Iblock\ElementTable::getList($parameters = array()); // получение элементов
+Iblock\ElementTable::getMap(); //возвращает описание карты сущностей
+Iblock\ElementTable::getRow($parameters); //возвращает один столбец (или null) по параметрам
+Iblock\ElementTable::getRowById($id); //возвращает один столбец (или null) по первичному ключу сущности
+Iblock\ElementTable::getTableName(); //возвращает имя таблицы БД для сущности
+Iblock\ElementTable::query(); //создаёт и возвращает объект запроса для сущности
+Iblock\ElementTable::update($primary, $data); // обновление элемента по ID
+Iblock\ElementTable::updateMulti($ids, $data, $ignoreEvents = false);
+Iblock\ElementTable::enableCrypto($field, $table = null, $mode = true); //устанавливает флаг поддержки шифрования для поля
+Iblock\ElementTable::cryptoEnabled($field, $table = null); //возвращает true если шифрование разрешено для поля
